d1e887940f6cd09fd286236fc1f87607b67572b5
Artem Mahaz <ipz221_mao@student.ztu.edu.ua>
2024-02-18 22:01:46 +0200

    Sum of 2 numbers

---
+++ ForPracTask0/ForPracTask0/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+      
+        Console.Write("Перше число: ");
+        if (double.TryParse(Console.ReadLine(), out double number1))
+        {
+          
+            Console.Write("Друге число: ");
+            if (double.TryParse(Console.ReadLine(), out double number2))
+            {
+               
+                double sum = number1 + number2;
+
+                
+                Console.WriteLine($"Сума чисел {number1} та  {number2} : {sum}");
+            }
+            else
+            {
+                Console.WriteLine("Некоректне друге число.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Некоректне перше число.");
+        }
+
+        Console.ReadLine();
+    }
+}
